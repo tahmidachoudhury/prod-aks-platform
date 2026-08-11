d0d7e49c02e3e395f49f926862064e2c4c0a6324
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2026-08-11 22:57:14 +0100

    fix: kubernetes output var moved to correct module

--- infra/platform/modules/kubernetes/outputs.tf
+++ infra/platform/modules/kubernetes/outputs.tf
@@ -6,3 +6,8 @@ output "oidc_issuer_url" {
 output "cluster_name" {
   value = azurerm_kubernetes_cluster.main.name
 }
+
+output "kubelet_identity_object_id" {
+  description = "Object ID of the kubelet identity. Requires AcrPull on the registry to pull images."
+  value       = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
--- infra/platform/outputs.tf
+++ infra/platform/outputs.tf
@@ -14,8 +14,3 @@ output "dns_zone_name" {
   description = "DNS zone for the ExternalDNS --domain-filter argument."
   value       = var.dns_zone_name
 }
-
-output "kubelet_identity_object_id" {
-  description = "Object ID of the kubelet identity. Requires AcrPull on the registry to pull images."
-  value       = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
-}
